7b46cec9cff45aff6799ad606d4ce6714ea9dd68
Gultomganteng <88914953+Gultomganteng@users.noreply.github.com>
2021-11-09 22:00:05 +0700

    Update sample_config.py

--- NaoRobot/sample_config.py
+++ NaoRobot/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Kittu"
+    OWNER_ID = 1744762756  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Tom_gultom"
     SUPPORT_CHAT = "Kora_Support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001598331506
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001315212341
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
